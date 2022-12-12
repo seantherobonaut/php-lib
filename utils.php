1ba5a344410d76b9560845da154662d980e1ad7c
Sean Leapley <seantherobonaut@gmail.com>
2022-12-12 19:25:29 +0000

    Renamed push_log -> append_file, fixed mkdir issue

--- utils.php
+++ utils.php
@@ -1,10 +1,12 @@
 <?
-    //File with a collection of useful functions
+    /*File with a collection of useful functions*/
 
-    function push_log($file, $data)
+    //Adds content to a file, ensures folder exists
+    function append_file($file, $data)
     {
-        if(!file_exists($file))
-                mkdir(pathinfo($file)['dirname']);
+        $path = pathinfo($file)['dirname'];
+        if(!file_exists($path))
+            mkdir($path);
         
         $targetFile = fopen($file, 'a');
         fwrite($targetFile, $data);

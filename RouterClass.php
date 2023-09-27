be205cdc21ed653978b192f727b15acf8831685c
Sean Leapley <seantherobonaut@gmail.com>
2023-09-26 20:59:44 -0700

    Updated path routing with regex

--- RouterClass.php
+++ RouterClass.php
@@ -1,47 +1,31 @@
 <?php
     //Basic path router
-    //For each call, return: content type, output, http status code
     class Router
-    {
+    {        
         private $routes = array();
 
-        public function __construct()
-        {
-            //Set default 404 for GET
-            $this->routes['GET']['404'] = function()
-            {
-                http_response_code(404);
-                echo "Error 404: File not found";
-            };
-
-            //Set default 404 for POST
-            $this->routes['POST']['404'] = function()
-            {
-                http_response_code(404);
-                echo "Error 404: File not found";
-            };        
-        }
-        
-        //Add paths with a callback to GET
+        //Add routes with a callback to GET
         public function get($route, $callback)
         {
-            $this->routes['GET'][$route] = $callback;
+            $this->routes['GET']['/'.trim(strtolower($route), '/').'/'] = $callback;
         }
         
-        //Add paths with a callback to POST
+        //Add routes with a callback to POST
         public function post($route, $callback)
         {
-            $this->routes['POST'][$route] = $callback;
+            $this->routes['POST']['/'.trim(strtolower($route), '/').'/'] = $callback;
         }
 
+        //Add routes with a callback to PUT
         public function put($route, $callback)
         {
-            $this->routes['PUT'][$route] = $callback;
+            $this->routes['PUT']['/'.trim(strtolower($route), '/').'/'] = $callback;
         }
-        
+
+        //Add routes with a callback to DELETE
         public function delete($route, $callback)
         {
-            $this->routes['DELETE'][$route] = $callback;
+            $this->routes['DELETE']['/'.trim(strtolower($route), '/').'/'] = $callback;
         }
 
         //Call an anonymous function matching request_uri and request_method from $routes
@@ -49,35 +33,60 @@
         {
             //format the input
             $method = $_SERVER['REQUEST_METHOD'];                
-            $query = (isset($_SERVER['QUERY_STRING'])) ? '?'.$_SERVER['QUERY_STRING'] : '';
-            $uri = $_SERVER['REQUEST_URI'];
-            $route = strtolower(str_replace($query,'',$uri));
-            
-            //if the route is not just '/'    
-            if(strlen($route)>1)
-            {
-                //Erase 1st forward slash from path
-                if($route[0]=="/")
-                    $route = substr($route,1,strlen($route)-1);
+            $queryString = (isset($_SERVER['QUERY_STRING'])) ? '?'.$_SERVER['QUERY_STRING'] : '';
+            $path = '/'.trim(strtolower(str_replace($queryString,'',$_SERVER['REQUEST_URI'])), '/').'/';
+                         
+            //check if the array for that request method exists
+            if(isset($this->routes[$method]))
+            {                
+                //find routes that match part of the url
+                $routematches = array();
+                foreach($this->routes[$method] as $route => $callback)
+                {
+                    $pattern = "/^".preg_quote($route,"/")."/i";
+
+                    if(preg_match($pattern, $path)) 
+                        $routematches[$route] = $callback;
+                }
+
+                //sort results on key length where largest is first
+                uksort($routematches, function($a,$b)
+                {
+                    if(strlen($a) == strlen($b))
+                    return 0;
                 
-                //Convert path to an array
-                $route = explode("/", $route);
+                    return (strlen($a) > strlen($b)) ? -1 : 1;
+                });    
+
+                //pass left over route data to callback function
+                if(count($routematches)>0)
+                {
+                    $matchKey = array_keys($routematches)[0];
+                    
+                    $matchCallback = array_shift($routematches);
+                    
+                    //trim route(only first occurrence) from path
+                    $trimmed = trim(implode("", explode($matchKey, $path, 2)), '/');
+                    //convert remaining string to an array split by '/'
+                    $route_data = (!empty($trimmed)) ? explode("/", $trimmed) : array();
+
+                    call_user_func($matchCallback, $route_data); 
+                }
+                else
+                {
+                    if(isset($this->routes[$method]['/'.'404'.'/']))
+                        call_user_func($this->routes[$method]['/'.'404'.'/'], $route);
+                    else
+                    {
+                        http_response_code(404);
+                        echo 'Route not found.';
+                    }
+                }
             }
             else
-                $route = array('page','home');
-
-            //Get the first node off of route (array pop)
-            $node = array_shift($route);
-
-            //Call and pass path data to anonymous functions
-            if(isset($this->routes[$method][$node]))
             {
-                call_user_func($this->routes[$method][$node], $route);                
-            }
-            else
-            {
-                if(isset($this->routes[$method]['404']))
-                    call_user_func($this->routes[$method]['404'], $route);
+                http_response_code(404);
+                echo 'Method not found.';
             }
         }
     }
